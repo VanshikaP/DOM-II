12c84e267b400e3f2f99b16fbe7ddddcc062770c
VanshikaP <vanshikapundir92@gmail.com>
2020-02-11 15:38:40 -0800

    Event Listeners Added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const navbar = document.querySelector('.main-navigation');
+navbar.addEventListener('mouseenter', () => {
+    navbar.style['background-color'] = 'rgb(32,178,170)';
+});
+navbar.addEventListener('mouseleave', () => {
+    navbar.style.removeProperty('background-color');
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(nav => {
+    nav.addEventListener('mouseenter', () =>{
+        nav.style.color = 'white';
+    });
+    nav.addEventListener('mouseleave', () =>{
+        nav.style.removeProperty('color');
+    });
+});
+
+const content = document.querySelector('body');
+window.addEventListener('resize', () => {
+    content.style['background-image'] = 'linear-gradient(to bottom right, papayawhip, lightseagreen)';
+    content.style.color = 'white';
+});
+
+const home = document.querySelector('.home');
+window.addEventListener('scroll', () => {
+    home.style['background-image'] = 'linear-gradient(to bottom right, lightseagreen, white)';
+});
